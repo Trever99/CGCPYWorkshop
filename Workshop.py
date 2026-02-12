a639d6924d014a2d0c4fe949f0a0acd0dc5ceb2f
Trever99 <skmariro@gmail.com>
2026-02-12 11:48:36 +0530

    Trever99 is cooking

---
+++ Workshop.py
@@ -0,0 +1,14 @@
+
+# print("Welcome to the Workshop!")
+
+name = "Emmanuel"
+school = "CGC"
+
+print(name)
+
+name = "Emash Sing"
+print(name)
+print(school)
+
+newname=input("What is your name? ")
+print("Hello " + newname + "! Welcome to the Workshop!")
