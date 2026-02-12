070ed8e061141c7b638eab1081fec7971d5124ba
Trever99 <skmariro@gmail.com>
2026-02-12 12:30:49 +0530

    Next Commit

---
+++ workshopDataTypes.py
@@ -0,0 +1,25 @@
+import math
+
+#datatypes
+#int = 1,2,3,4
+#float = 1.0, 2.0, 3.0
+#string = "hello", 'world'
+
+
+
+
+#Data structures 
+#list = [1,2,3,4]
+#tuple = (1,2,3,4)
+#dictionary = {"key1": "value1", "key2": "value2"}
+#set = {1,2,3,4}
+
+
+# first = 4
+# r = 5
+# square_first = first * first
+# area = math.pi * r * r
+
+radii = input("Enter the radius: ")
+area = math.pi * float(radii) * float(radii)
+print(f"The area of the circle is: {area}")
